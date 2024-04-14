6a691938c7d667152833749434fca80df1d6baff
Prateek Jadhav <134202260+Prateek0712@users.noreply.github.com>
2024-04-14 14:14:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function firstNonRepeatedChar(str) {
- // Write your code here
+ // Write your code her
+	let freqArr=[];
+	for(let i=0;i<26;i++)
+		{
+		  freqArr[i]=0;
+		}
+	for(let  ch of str)
+		{
+			freqArr[ch.charCodeAt(0) - 'a'.charCodeAt(0)]+=1;
+			console.log(ch.charCodeAt(0) - 'a'.charCodeAt(0))
+			
+		}
+	for(let ch of str)
+		{
+			if(freqArr[ch.charCodeAt(0) - 'a'.charCodeAt(0)]==1)
+			{
+				return ch;
+			}
+		}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
